06f1e6a62d378ff0f018a0dc080bd26a71a920c7
Noxitu <grzesiug44+github@gmail.com>
2020-10-22 20:58:30 +0000

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,38 @@
 
 namespace noxitu::vulkan
 {
+    vk::PhysicalDevice findPhysicalDevice(const std::vector<vk::PhysicalDevice> &physicalDevices,
+                                        std::function<bool(const vk::PhysicalDevice&)> condition)
+    {
+        auto it = std::find_if(physicalDevices.begin(), physicalDevices.end(), condition);
+
+        if (it == physicalDevices.end())
+            throw std::runtime_error("No physical device found");
+
+        return *it;
+    }
+
+    class QueueFamilyIndexFinder
+    {
+    private:
+        std::vector<vk::QueueFamilyProperties> m_queueFamiliyProperties;
+
+    public:
+        QueueFamilyIndexFinder(const vk::PhysicalDevice &physicalDevice) :
+            m_queueFamiliyProperties(physicalDevice.getQueueFamilyProperties())
+        {}
+
+        int find(const std::function<bool(const vk::QueueFamilyProperties&)> &condition) const
+        {
+            auto it = std::find_if(m_queueFamiliyProperties.begin(), m_queueFamiliyProperties.end(), condition);
+
+            if (it == m_queueFamiliyProperties.end())
+                throw std::runtime_error("No valid queue family");
+
+            return std::distance(m_queueFamiliyProperties.begin(), it);
+        }
+    };
+
     class InstanceBuilder
     {
     private:
@@ -50,20 +82,12 @@ namespace noxitu::vulkan
 
         std::pair<vk::Device, vk::Queue> createDevice(const vk::PhysicalDevice &physicalDevice)
         {
-            const int queueFamilyIndex = [&]()
-            {
-                const auto queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
-
-                for (int i = 0; i < static_cast<int>(queueFamilyProperties.size()); ++i)
+            const int queueFamilyIndex = QueueFamilyIndexFinder(physicalDevice).find(
+                [](const vk::QueueFamilyProperties &properties)
                 {
-                    const vk::QueueFamilyProperties &properties = queueFamilyProperties.at(i);
-
-                    if (properties.queueCount > 0 && (properties.queueFlags & vk::QueueFlagBits::eCompute))
-                        return i;
+                    return properties.queueCount > 0 && (properties.queueFlags & vk::QueueFlagBits::eCompute);
                 }
-
-                throw std::runtime_error("No valid queue family");
-            }();
+            );
 
             std::vector<vk::DeviceQueueCreateInfo> queueInfos;
 
@@ -92,11 +116,44 @@ namespace noxitu::vulkan
     };
 }
 
+void printPhysicalDevices(const std::vector<vk::PhysicalDevice> &physicalDevices)
+{
+    std::cout << "Found Physical Devices:\n";
+
+    for (const vk::PhysicalDevice &device : physicalDevices)
+    {
+        const vk::PhysicalDeviceProperties properties = device.getProperties();
+
+        std::cout << " * " << properties.deviceName << '\n';
+    }
+
+    std::cout << std::endl;
+}
+
+class DebugReportCallback
+{
+public:
+    bool operator()(VkDebugReportFlagsEXT,
+                    VkDebugReportObjectTypeEXT,
+                    uint64_t,
+                    size_t,
+                    int32_t,
+                    const char* layerPrefix,
+                    const char* message) const
+    {
+        std::cerr << "Vulkan Debug(layerPrefix=\"" << layerPrefix << "\""
+                  << "             message=\"" << message << "\")"
+                  << std::endl;
+
+        return false;
+    }
+};
+
 int main(const int, const char* const[]) try
 {
     noxitu::vulkan::InstanceBuilder builder;
     
-    const bool enable_validation_layer = false && noxitu::vulkan::can_enable_validation_layer();
+    const bool enable_validation_layer = noxitu::vulkan::can_enable_validation_layer();
 
     if (enable_validation_layer)
         builder.enable_validation_layer();
@@ -105,47 +162,21 @@ int main(const int, const char* const[]) try
 
     const auto instance = builder.createInstance();
 
-    auto callback = [&](
-        VkDebugReportFlagsEXT,
-        VkDebugReportObjectTypeEXT,
-        uint64_t,
-        size_t,
-        int32_t,
-        const char* pLayerPrefix,
-        const char* pMessage
-    ) {
-        std::cerr << "Vulkan Debug(layerPrefix=\"" << pLayerPrefix << "\""
-                  << "             message=\"" << pMessage << "\")"
-                  << std::endl;
-
-        return false;
-    };
+    DebugReportCallback debugCallback;
 
     if (enable_validation_layer)
-        noxitu::vulkan::createDebugCallback(instance, &callback);
+        noxitu::vulkan::createDebugCallback(instance, &debugCallback);
 
-    const auto devices = instance.enumeratePhysicalDevices();
-
-    for (const vk::PhysicalDevice &device : devices)
+    const vk::PhysicalDevice physicalDevice = [&]()
     {
-        const vk::PhysicalDeviceProperties properties = device.getProperties();
-
-        std::cout << " * " << properties.deviceName << std::endl;
-    }
-
-    const vk::PhysicalDevice selectedDevice = [&]()
-    {
-        auto it = std::find_if(devices.begin(), devices.end(), [](auto&) {return true;});
-
-        if (it == devices.end())
-            throw std::runtime_error("No physical device found");
-
-        return *it;
+        const auto physicalDevices = instance.enumeratePhysicalDevices();
+        printPhysicalDevices(physicalDevices);
+        return noxitu::vulkan::findPhysicalDevice(physicalDevices, [](auto&) {return true;});
     }();
 
-    std::cout << "Using device: " << selectedDevice.getProperties().deviceName << std::endl;
+    std::cout << "Using device: " << physicalDevice.getProperties().deviceName << std::endl;
 
-    const auto [device, queue] = builder.createDevice(selectedDevice);
+    const auto [device, queue] = builder.createDevice(physicalDevice);
 
     std::cerr << "main() done" << std::endl;
     return EXIT_SUCCESS;
